c0c2fff3eb62ee97eca5dd42be2146bed254c597
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-05-04 18:48:51 +0530

    api work properly

---
+++ src/API/server.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 5050;
+
+// // Dummy JSON data
+// const chatMessages = require('./api.json');
+
+// // Define route to retrieve chat messages
+// app.get('/api/messages', (req, res) => {
+//   res.json(chatMessages);
+// });
+
+// // Start server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
